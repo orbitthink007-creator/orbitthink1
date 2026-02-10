015d43ecf2c02144b450bfa262f106f770eadac2
tulaibtechnyx <tulaib.siddiqui@technyxsystems.com>
2026-02-10 16:26:00 +0500

    Work for button and other

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./globals.css";
